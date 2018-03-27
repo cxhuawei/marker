629a3cedeede9a16f0a237a7122791ac912f5713
cxhuawei <xuchen1990xx@gmail.com>
2018-03-27 01:32:09 -0700

    delete useless lib

--- marker/task/engine.py
+++ marker/task/engine.py
@@ -2,7 +2,6 @@ import json
 import os
 import time
 
-from marker.api import API
 from marker.common import logging
 from marker.common.objects import targetandtask
 from marker.probes.base_probes import BaseProbes
